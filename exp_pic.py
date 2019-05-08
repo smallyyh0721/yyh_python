8e155e65628eeac653fcd75d142a876ecd002a57
smallyyh0721 <50401896+smallyyh0721@users.noreply.github.com>
2019-05-08 18:03:38 +0800

    Update

--- exp_pic.py
+++ exp_pic.py
@@ -6,11 +6,12 @@ import re
 import requests
 
 
-def dowmloadPic(html, keyword):
+def dowmloadPic(html, keyword,count):
     pic_url = re.findall('"objURL":"(.*?)",', html, re.S)
     i = 1
-    count=12
+#    count=12
     print('找到关键词:' + keyword + '的图片，现在开始下载图片...')
+    
     for each in pic_url:
 #        print(each)
         print('正在下载第' + str(i) + '张图片，图片地址:' + str(each))
@@ -33,7 +34,10 @@ def dowmloadPic(html, keyword):
 
 if __name__ == '__main__':
     word = input("Input key word: ")
-    #url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + word + '&ct=201326592&v=flip'
+    countpic = int(input("Input picture amount: "))
+ #   url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + word + '&ct=201326592&v=flip'
     url = 'http://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&fm=index&pos=history&word='+word
+    print(url)
     result = requests.get(url)
-    dowmloadPic(result.text, word)
+    print(result)
+    dowmloadPic(result.text, word,countpic)
