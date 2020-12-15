e78dce145154e727e1d534e11ab17a48702d282f
smallyyhhahaha <50401896+smallyyh0721@users.noreply.github.com>
2020-12-15 22:00:06 +0800

    Add files via upload

--- GetSHnew300_500.py
+++ GetSHnew300_500.py
@@ -3,6 +3,13 @@
 Created on Fri Dec 11 00:06:19 2020
 
 @author: yongh
+
+Next Step :
+    分析楼盘的详细信息
+    https://sh.fang.lianjia.com/loupan/p_jfcafsam/xiangqing/
+    获取开盘信息（首页） 规划信息 配套信息（详情）
+    规划信息
+    
 """
 
 import bs4,requests
@@ -15,7 +22,10 @@ from bs4 import BeautifulSoup
 
 #Test : Get all items from page 1  Total 300 - 500
 
+page = "https://sh.fang.lianjia.com/loupan/"
 Tpage = "https://sh.fang.lianjia.com/loupan/bp300ep500pg"
+xq = "xiangqing"
+
 LP_name = []
 CSV_headers = ["Name","Distric","Addr","Area","UnitP","TotalP"]
 
@@ -31,34 +41,44 @@ def getLPinfo(LPlist,url):
     
 
     for lp in lps:
+        # Start Test Process
+        
+        test = lp.findChild().get_attribute_list("href")
+        print("Test is :",test)
+        web=page + test[0] +xq
+        print(web)
+        print("status is :",(requests.get(web)).status_code)
+        
+        #End Test Process
+        
         DICTtmp = {"Name": "", "Distric":"" ,"Addr":"", "Area":"", "UnitP":"", "TotalP":""}
-        print("Found a new loupan\n")
+        #print("Found a new loupan\n")
         tmp=lp.findChild().findNextSibling()
         infos=tmp.findChild()   
         DICTtmp["Name"] = infos.a.get_text()
         #DICTtmp["Name"]  = '\ufeff' +"实验"
         #print("loupan Name: ",infos.a.get_text())
-        print("loupan name is :",DICTtmp["Name"])
+        #print("loupan name is :",DICTtmp["Name"])
         
         infos=infos.findNextSibling()
         DICTtmp["Distric"] = infos.span.get_text()         
         #print("loupan Distric: ",infos.span.get_text())
-        print("loupan District",DICTtmp["Distric"])
+        #print("loupan District",DICTtmp["Distric"])
         DICTtmp["Addr"] = infos.a.get_text()
         #print("loupan Addr: ",infos.a.get_text())
-        print("loupan Addr:",DICTtmp["Addr"])
+        #print("loupan Addr:",DICTtmp["Addr"])
         infos=infos.findNextSibling().findNextSibling()
         DICTtmp["Area"] = infos.span.get_text()
         #print("loupan Area: ",infos.span.get_text())
-        print("loupan Area: ",DICTtmp["Area"])
+        #print("loupan Area: ",DICTtmp["Area"])
         infos=infos.findNextSibling().findNextSibling().findNextSibling()
         DICTtmp["UnitP"] = infos.div.span.get_text()
         #print("loupan Single Price/m2: ",infos.div.span.get_text())
-        print("loupan Single Price/m2: ",DICTtmp["UnitP"])
+        #print("loupan Single Price/m2: ",DICTtmp["UnitP"])
         for tp in infos.findNext().fetchNextSiblings():
             DICTtmp["TotalP"] = tp.get_text()
             #print("loupan total Price: ",tp.get_text())
-            print("loupan total Price: ",DICTtmp["TotalP"])
+            #print("loupan total Price: ",DICTtmp["TotalP"])
         #DICTnew = DICTtmp
         
         LPlist.append(DICTtmp)
@@ -106,11 +126,11 @@ if __name__=="__main__":
     #print("LP is :\n",LP)
     """
     #把楼盘信息写入文件
-    for i in range(30):
+    for i in range(1):
         newpage = Tpage+str(i+1)
         
         LP = getLPinfo(LP_name,newpage)
-        write2csv(LP,csvfile)
+        #write2csv(LP,csvfile)
     
     #生成
     #print("Get Loupan info :\n",LP)
